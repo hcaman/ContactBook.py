c2b0a019448989c2d304ae70b374afaae4de738c
hugo cm <hugocmdesign@gmail.com>
2021-09-29 22:28:03 +0200

    Import os and make a fn for create the folder

--- proyecto.py
+++ proyecto.py
@@ -1,6 +1,18 @@
+import os
+
+# Variables goblales
+CARPETA = 'contactos/'
+
 def app():
+    # Revisa si la carpeta existe, sino la crea
+    crear_directorio()
     # Muestra el menu de opciones
     mostrar_menu()
+
+def crear_directorio():
+    if not os.path.exists(CARPETA):
+        os.makedirs(CARPETA)
+
 
 def mostrar_menu():
     print('Selecciones del menu:')
